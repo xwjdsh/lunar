c86239c17886991a4d31d76c180cde04b9e2a938
Wendell Sun <iwendellsun@gmail.com>
2021-07-27 01:53:19 +0800

    add benchmark test

--- lunar_test.go
+++ lunar_test.go
@@ -2,38 +2,96 @@ package lunar
 
 import (
 	"testing"
-	"time"
 )
 
-var m = map[string]string{
-	"20210720": "20210611",
-	"19950101": "19941201",
-	"20220201": "20220101",
-	"20880919": "20880805",
+var m = map[Date]Date{
+	{2021, 7, 20}:  {2021, 6, 11},
+	{1995, 1, 1}:   {1994, 12, 1},
+	{2022, 2, 1}:   {2022, 1, 1},
+	{2088, 9, 19}:  {2088, 8, 5},
+	{2088, 12, 19}: {2088, 11, 7},
 }
 
 func TestDateToLunarDate(t *testing.T) {
-	for k, v := range m {
-		req, _ := time.Parse("20060102", k)
-		d, err := DateToLunarDate(DateByTime(req))
-		if err != nil {
-			t.Error(err)
+	for i := 0; i <= 1; i++ {
+		if i == 1 {
+			Cache(true)
 		}
-		if actual := d.LunarDate.Time().Format("20060102"); actual != v {
-			t.Errorf("DateToLunarDate error, expected: %s, actual: %s", v, actual)
+
+		for k, v := range m {
+			d, err := DateToLunarDate(k)
+			if err != nil {
+				t.Error(err)
+			}
+			if actual := d.LunarDate; actual != v {
+				t.Errorf("DateToLunarDate error, expected: %s, actual: %s", v, actual)
+			}
+		}
+	}
+}
+
+func BenchmarkDateToLunarDate(b *testing.B) {
+	h := New()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		for k, v := range m {
+			r, _ := h.DateToLunarDate(k)
+			if r.LunarDate != v {
+				b.FailNow()
+			}
+		}
+	}
+}
+
+func BenchmarkDateToLunarDateCache(b *testing.B) {
+	h := New()
+	h.Cache(true)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		for k, v := range m {
+			r, _ := h.DateToLunarDate(k)
+			if r.LunarDate != v {
+				b.FailNow()
+			}
 		}
 	}
 }
 
 func TestLunarDateToDate(t *testing.T) {
 	for k, v := range m {
-		req, _ := time.Parse("20060102", v)
-		d, err := LunarDateToDate(DateByTime(req))
+		d, err := LunarDateToDate(v)
 		if err != nil {
 			t.Error(err)
 		}
-		if actual := d.Date.Time().Format("20060102"); actual != k {
+		if actual := d.Date; actual != k {
 			t.Errorf("LunarDateToDate error, expected: %s, actual: %s", k, actual)
+		}
+	}
+}
+
+func BenchmarkLunarDateToDate(b *testing.B) {
+	h := New()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		for k, v := range m {
+			r, _ := h.LunarDateToDate(v)
+			if r.Date != k {
+				b.FailNow()
+			}
+		}
+	}
+}
+
+func BenchmarkLunarDateToDateCache(b *testing.B) {
+	h := New()
+	h.Cache(true)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		for k, v := range m {
+			r, _ := h.LunarDateToDate(v)
+			if r.Date != k {
+				b.FailNow()
+			}
 		}
 	}
 }
